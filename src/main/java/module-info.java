32869199474fab7f37b62aaabc3e382e88ae2731
Daniel Gandolfi <danielgandolfi.10@gmail.com>
2022-11-11 00:25:16 -0300

    feat: create system bank

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module project.system_bank {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens project.system_bank to javafx.fxml;
+    exports project.system_bank;
+}
