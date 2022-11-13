cc6492f19e31901959ccd8fd2d6d6efe7992bae0
lucasvrielink <lucaasvrielink@gmail.com>
2022-11-13 19:21:56 -0300

    Renomeação de pastas e delete de pastas que não faziam sentido.

--- src/main/project/system_bank/BankApp.java
+++ src/main/project/system_bank/BankApp.java
@@ -4,11 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import ConnectionDB.ConexaoDB;
 import java.io.IOException;
-import java.sql.Connection;
-
-import static ConnectionDB.ConexaoDB.getConnection;
 
 public class BankApp extends Application {
     @Override
@@ -20,6 +16,6 @@ public class BankApp extends Application {
     }
 
     public static void main(String[] args) {
-        Connection conn = ConexaoDB.getConnection();
+        launch();
     }
 }
