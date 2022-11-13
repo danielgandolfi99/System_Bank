ee20d6be57531f27d1623e6796a39a70559187e4
Daniel Gandolfi <danielgandolfi.10@gmail.com>
2022-11-13 16:27:54 -0300

    fix: Criação correta do projeto

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,13 +3,6 @@ module project.system_bank {
     requires javafx.fxml;
     requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
-
     opens project.system_bank to javafx.fxml;
     exports project.system_bank;
 }
