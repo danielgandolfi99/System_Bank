3532324593915194d0b0f88a706a183abe04ecb9
Daniel Gandolfi <danielgandolfi.10@gmail.com>
2022-11-19 14:31:01 -0300

    feat: Criação das interfaces

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module project.system_bank {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
 
     opens project.system_bank to javafx.fxml;
     exports project.system_bank;
--- src/main/java/project/system_bank/home_login.java
+++ src/main/java/project/system_bank/home_login.java
@@ -2,79 +2,46 @@ package project.system_bank;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
 
 public class home_login {
-
-    @FXML
-    private TextField password;
-
-    @FXML
-    private Button login_action;
 
     @FXML
     private TextField login;
 
     @FXML
-    private Label create_account;
+    private PasswordField password;
 
-    public TextField getPassword() {
-        return password;
-    }
+    @FXML
+    void setLogin(ActionEvent event) {
 
-    public void setPassword(TextField password) {
-        this.password = password;
-    }
-
-    public Button getLogin_action() {
-        return login_action;
-    }
-
-    public void setLogin_action(Button login_action) {
-        this.login_action = login_action;
-    }
-
-    public TextField getLogin() {
-        return login;
-    }
-
-    public void setLogin(TextField login) {
-        this.login = login;
-    }
-
-    public Label getCreate_account() {
-        return create_account;
-    }
-
-    public void setCreate_account(Label create_account) {
-        this.create_account = create_account;
-    }
-
-    @Override
-    public String toString() {
-        return "home_login{" +
-                "password=" + password +
-                ", login_action=" + login_action +
-                ", login=" + login +
-                ", create_account=" + create_account +
-                '}';
     }
 
 
     @FXML
-    void login_action(ActionEvent event){
-
+    void setPassword(ActionEvent event) throws IOException {
+        login_action(event);
     }
 
     @FXML
-    void get_login(ActionEvent event) {
-        System.out.println(login);
+    void login_action(ActionEvent event) throws IOException {
+
+        Login_Home user = new Login_Home(login.getText(),password.getText());
+        System.out.println(user);
+        //BankApp.getStage().close();
     }
 
     @FXML
-    void get_password(ActionEvent event) {
-        getPassword();
+    void createAccount(ActionEvent event) throws IOException {
+        BankApp.createAccount();
+        BankApp.getStage().close();
     }
 }
