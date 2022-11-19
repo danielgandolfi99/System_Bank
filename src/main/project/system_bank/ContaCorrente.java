ad986bef961ad6770c5caa61130be7db5033cd92
lucasvrielink <lucaasvrielink@gmail.com>
2022-11-19 11:17:00 -0300

    Renomeação de pastas e delete de pastas que não faziam sentido.

---
+++ src/main/project/system_bank/ContaCorrente.java
@@ -0,0 +1,36 @@
+package project.system_bank;
+
+public class ContaCorrente extends Conta{
+
+    public ContaCorrente(int conta_id, int agencia, double saldo, String usuario, String senha) {
+        super(conta_id, agencia, saldo, usuario, senha);
+    }
+
+    @Override
+    public boolean saca(double valor) {
+        if(super.getSaldo() < valor) {
+           return false;
+        }
+        super.setSaldo(super.getSaldo()-valor);
+        return true;
+    }
+
+    @Override
+    public boolean deposita(double valor) {
+        if(valor<0.0){
+            return false;
+        }
+        super.setSaldo(super.getSaldo()+valor);
+        return true;
+    }
+
+    public boolean transfere(double valor, Conta conta){
+        if(valor>(super.getSaldo()*1.05) || valor<0){
+            return false;
+        }
+        super.setSaldo(super.getSaldo()-valor);
+        conta.setSaldo(conta.getSaldo()+valor);
+        return true;
+    }
+
+}
