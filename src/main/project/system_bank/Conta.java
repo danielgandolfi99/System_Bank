ccc5572dcf5b30c732b831d9567f4a5c31bd0e2f
lucasvrielink <lucaasvrielink@gmail.com>
2022-11-19 11:16:38 -0300

    Renomeação de pastas e delete de pastas que não faziam sentido.

--- src/main/project/system_bank/Conta.java
+++ src/main/project/system_bank/Conta.java
@@ -5,33 +5,62 @@ import java.util.Objects;
 public abstract class Conta {
     private int conta_id;
     private int agencia;
-    private int saldo;
-    private int tipo;
-    private int usuario;
-    private int senha;
+    private double saldo;
+    private String usuario;
+    private String senha;
 
 
-    public abstract int getConta_id();
+    public Conta(int conta_id, int agencia, double saldo, String usuario, String senha) {
+        this.conta_id = conta_id;
+        this.agencia = agencia;
+        this.saldo = saldo;
+        this.usuario = usuario;
+        this.senha = senha;
+    }
 
-    public abstract void setConta_id(int conta_id);
+    public abstract boolean saca(double valor);
 
-    public abstract int getAgencia();
+    public abstract boolean deposita(double valor);
 
-    public abstract void setAgencia(int agencia);
+    public int getConta_id() {
+        return conta_id;
+    }
 
-    public abstract int getSaldo();
+    public void setConta_id(int conta_id) {
+        this.conta_id = conta_id;
+    }
 
-    public abstract void setSaldo(int saldo);
+    public int getAgencia() {
+        return agencia;
+    }
 
-    public abstract int getTipo();
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
 
-    public abstract void setTipo(int tipo);
+    public double getSaldo() {
+        return saldo;
+    }
 
-    public abstract int getUsuario();
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 
-    public abstract void setUsuario(int usuario);
+    public String getUsuario() {
+        return usuario;
+    }
 
-    public abstract int getSenha();
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
 
-    public abstract void setSenha(int senha);
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
 }
